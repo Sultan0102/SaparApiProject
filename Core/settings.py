5bbce4c10c3db8cbdc7c9428b724fcd3bef3f860
soTEKTy <jora.bakytbek0808@gmail.com>
2022-12-15 21:46:09 +0600

    Added UI. Profile page

--- Core/settings.py
+++ Core/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SaparAPI',
         'USER': 'postgres',
-        'PASSWORD': '1320',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
     }
