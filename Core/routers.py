1ef8ac5ae74452d137563b74a2d4cc984aec94ed
Sultan0102 <sulnan9@gmail.com>
2023-02-15 18:12:58 +0600

    Added filtering, ordering and localization for routes

--- Core/routers.py
+++ Core/routers.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import SimpleRouter, DefaultRouter
 from Core.users.views import UserViewSet
 from Core.authorization.views import LoginViewSet, RegistrationViewSet, RefreshViewSet
-from Core.tickets.views import RouteViewSet, LocationViewSet, PostTicketViewSet
+from Core.tickets.api import RouteViewSet, LocationViewSet, PostTicketViewSet, DetailRouteViewSet,DetailPostTicketViewSet
 
 routes = SimpleRouter()
+router = DefaultRouter()
+detRouter= DefaultRouter()
 
 # AUTHENTICATION
 routes.register(r'auth/login', LoginViewSet, basename='auth-login')
@@ -12,6 +14,13 @@ routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 # USER
 routes.register(r'users', UserViewSet, basename='users')
+
+# TICKETS
+router.register('routes',RouteViewSet,basename='routes')
+router.register('location',LocationViewSet, basename='location')
+router.register('post_ticket',PostTicketViewSet,basename='post_ticket')
+detRouter.register('routes',DetailRouteViewSet,basename='routes')
+detRouter.register('post_ticket',DetailPostTicketViewSet,basename='post_ticket')
 
 urlpatterns = [
     *routes.urls
